1540a2317e38b645ce54f6c4939118b2e742e9e8
Halleluyah <halleluyah1000000@gmail.com>
2023-07-12 14:05:55 +0900

    Extended for more input arguments

--- executable/4wayJunction.py
+++ executable/4wayJunction.py
@@ -3,7 +3,7 @@ import csv
 import re
 import math
 import argparse
-import sys
+
 
 def strandIdentifierL(j):
 
@@ -48,7 +48,7 @@ def strandIdentifierL(j):
     
     half1 = divide_string(input1)
     half2 = divide_string(input2, reverse=True)
-    print(half1, half2)
+    # print(half1, half2)
     return half1, half2[::-1]
 
 
@@ -98,13 +98,46 @@ def write_to_csv(rows, out):
         writer.writerow(['Strand1', 'Strand2', 'Strand3', 'Strand4', 'Final Melting Temperature(degree Celsius)', 'Number of Mismatches', 'Mismatch Loc[Strand:Location on Strand]'])
         writer.writerows(rows)
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Description of your program')          
-    parser.add_argument('-P', '--ion_concentration', default='0.00000015', help='Ion concentration')
-    parser.add_argument('-E', '--electrolyte_concentration',  default='Na=1:Cl=1', help='Electrolyte concentration')
+    parser = argparse.ArgumentParser(description='Description of your program')
+    # new options
     parser.add_argument('-out', '--output_file',  default='output.csv', help='Output file name')
     parser.add_argument('-in', '--input_file',  default='input.csv', help='Input file name')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode')
     parser.add_argument('-sort', '--sort',  action='store_true', help='Sort the output file based on melting temperature')
+
+    #existing options
+    parser.add_argument('-E', '--electrolyte_concentration',  default='Na=1:Cl=1', help='Electrolyte concentration')          
+    parser.add_argument('-P', '--ion_concentration', default='0.00000015', help='Ion concentration')
+    parser.add_argument('-H', '--hybridization', default='dnadna', help='Hytbridization type')
+    # parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode')
+        # verbose option isn't useless for our case
+    parser.add_argument('-T', '--threshold_value', default='60', help='Threshold for approximative computation')
+    # parser.add_argument('-nnpath', '--folder_pathway', default='Data', help='Change the default pathway where to find the default calorimetric tables')
+     # parser O option isn't implemented because our program doesn't use the option -O instead it uses -out and -in
+    parser.add_argument('-self', '--self_complementary', action='store_true', help='To precise that the sequence entered is self complementary')
+    parser.add_argument('-F', '--factor_value', default='4', help='Correction for the concentration of nucleic acid')
+    parser.add_argument('-am', '--approximative_formula', default='wetdna91', help='Specific approximative formula')
+    parser.add_argument('-nn', '--nearest_neighbor_model', default='all97', help='Specific nearest neighbor model')
+    parser.add_argument('-sinMM', '--sinMM_model', default='allsanpey', help='Specific nearest neighbor model for single mismatch(es)')
+    parser.add_argument('-GU', '--GU_model', default='ser12', help='Specific nearest neighbor model for GU base pairs')
+    # parser.add_argument('-tanMM', '--tanMM_model', default='allsanpey', help='Specific nearest neighbor model for tandem mismatches')
+    parser.add_argument('-intLP', '--intLP_model', default='san04', help='Specific nearest neighbor model for internal loop')
+    parser.add_argument('-sinDE', '--sinDE_model', default='bom00', help='Specific nearest neighbor model for single dangling end(s)')
+    parser.add_argument('-secDE', '--secDE_model', default='sugdna02', help='Specific nearest neighbor model for second dangling end(s)')
+    parser.add_argument('-lonDE', '--lonDE_model', default='sugdna02', help='Specific nearest neighbor model for long dangling end(s)')
+    parser.add_argument('-sinBU', '--sinBU_model', default='tan04', help='Specific nearest neighbor model for single bulge loop(s)')
+    parser.add_argument('-lonBU', '--lonBU_model', default='san04', help='Specific nearest neighbor model for long bulge loop(s)')
+    parser.add_argument('-CNG', '--CNG_model', default='bro05', help='Specific nearest neighbor model for RNA sequences composed of CNG repeats')
+    parser.add_argument('-ino', '--ino_model', default='san05', help='Specific nearest neighbor model for inosine base')
+    parser.add_argument('-ha', '--ha_model', default='sug01', help='Specific nearest neighbor model for hydroxyadenine base')
+    parser.add_argument('-azo', '--azo_model', default='asa05', help='Specific nearest neighbor model for azobenzene')
+    parser.add_argument('-lck', '--lck_model', default='owc11', help='Specific nearest neighbor model for locked nucleic acid')
+    parser.add_argument('-tanLck', '--tanLck_model', default='owc11', help='Specific nearest neighbor model for consecutive locked nucleic acid')
+    
+    parser.add_argument('-sinMMLck', '--sinMMLck_model', default='owc11', help='Specific nearest neighbor model for consecutive locked nucleic acid with one single mismatch')
+    parser.add_argument('-ion', '--ion_correction', default='owc2204', help='Specific ion correction')
+    parser.add_argument('-naeq', '--naeq_correction', default='ahs01', help='Specific ion correction which gives a sodium equivalent concentration')
+    parser.add_argument('-DMSO', '--DMSO_correction', default='ahs01', help='Specific DMSO correction')
+    parser.add_argument('-for', '--formamide_correction', default='bla96', help='Specific formamide correction')
     # parser.add_argument('-help', '--help', action='help', help='Show this help message and exit')
     args = parser.parse_args()
     rows = []
@@ -126,9 +159,39 @@ if __name__ == "__main__":
                 strandL, complement_strandL= strandIdentifierL(one_side)
 
 
-                command = ['./melting',"-S", strandL, 
-                                        "-C", complement_strandL, "-H", "dnadna",
-                                        '-P', args.ion_concentration, '-E', args.electrolyte_concentration, '-lck','owc11']
+                command = ['./melting', 
+                                        # '-v' if args.verbose else '', 
+                                        "-S", strandL, 
+                                        "-C", complement_strandL, 
+                                        "-H", args.hybridization,
+                                        '-P', args.ion_concentration, '-E', args.electrolyte_concentration, 
+                                        '-T', args.threshold_value,
+                                        # '-nnpath', args.folder_pathway,
+                                        '-self' if args.self_complementary else '',
+                                        '-F', args.factor_value,
+                                        '-am', args.approximative_formula,
+                                        '-nn', args.nearest_neighbor_model,
+                                        '-sinMM', args.sinMM_model,
+                                        '-GU', args.GU_model,
+                                        # '-tanMM', args.tanMM_model,
+                                        '-intLP', args.intLP_model,
+                                        '-sinDE', args.sinDE_model,
+                                        '-secDE', args.secDE_model,
+                                        '-lonDE', args.lonDE_model,
+                                        '-sinBU', args.sinBU_model,
+                                        '-lonBU', args.lonBU_model,
+                                        '-CNG', args.CNG_model,
+                                        '-ino', args.ino_model,
+                                        '-ha', args.ha_model,
+                                        '-azo', args.azo_model,
+                                        '-lck', args.lck_model,
+                                        '-tanLck', args.tanLck_model,
+                                        '-sinMMLck', args.sinMMLck_model,
+                                        '-ion', args.ion_correction,
+                                        '-naeq', args.naeq_correction,
+                                        '-DMSO', args.DMSO_correction,
+                                        '-for', args.formamide_correction
+                                        ]
                 # -nn sug96 gave out 35.1
                 #'-ion','tanna06' gave 37.13
                 #'-ion','ahs01' gave 41.86
@@ -143,8 +206,8 @@ if __name__ == "__main__":
                 
 
 
-                if(args.verbose):
-                    command.append('-v')
+                # if(args.verbose):
+                #     command.append('-v')
  
                 # print(command)
                 command_line = subprocess.run(command, 
@@ -171,5 +234,5 @@ if __name__ == "__main__":
             rows.append(row)
     if(args.sort):
         rows = sorted(rows, key=lambda x: (math.isnan(x[4]), x[4]), reverse=False) #if reverse is true then descending order
-    print(rows)
+    # print(rows)
     write_to_csv(rows, args.output_file)
